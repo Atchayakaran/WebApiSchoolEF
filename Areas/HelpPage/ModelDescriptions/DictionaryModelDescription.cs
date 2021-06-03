0f9d12696ebaddd768890247a53902bc5328e418
atchukaran21@gmail.com <atchukaran21@gmail.com>
2021-06-03 15:22:19 +0530

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiSchoolEF.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiSchoolEF.WebApiApplication" Language="C#" %>
